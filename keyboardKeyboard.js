2abdaa4eb6d50728653ea1808abdfc7820386033
Joshua <Jplittle2001@gmail.com>
2017-10-06 19:10:49 -0400

    working on polish and functionality

--- keyboardKeyboard.js
+++ keyboardKeyboard.js
@@ -1,11 +1,12 @@
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var keyOrder = 'qazwsxedcrfvtgbyhnujmik,ol.p;/[';
+var shiftKeyOrder = 'QAZWSXEDCRFVTGBYHNUJMIK<OL>P:?{"';
 var keyNames = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#']
 var waveTypes = ['square','sine','sawtooth','triangle']
 var waveSelected = 0;
 var rootFreq = 440.0;
 var duration = 100;
-var offsetOctave = 0;
+var shiftOffset = 11;
 
 function playNote(frequency, duration) {
     // create Oscillator node
@@ -24,12 +25,26 @@ function playNote(frequency, duration) {
 
 function updateInfo(){
     document.getElementById('durationRead').innerText = duration;
-    document.getElementById('offsetOctaveRead').innerText = offsetOctave;
     document.getElementById('waveTypeRead').innerText = waveTypes[waveSelected];
-
 }
 
-function log(text){
+function detectMobile() { 
+    if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    ){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function logKey(text, octive){
     document.getElementById('log').innerHTML += ('<span class="mono">' + text + '</span>');
     window.scrollTo(0,document.body.scrollHeight);
 }
@@ -37,15 +52,26 @@ function log(text){
 function runKey(key){
     console.log(key);
 
-    var num = keyOrder.indexOf(key.key);
-    if (num > -1){
-        freq = rootFreq * Math.pow(Math.pow(2,1/12),num+offsetOctave*11);
-        playNote(Math.floor(freq),duration);
-        //log('' + Math.floor(num/keyNames.length) + keyNames[num%keyNames.length] + '  ');
-        log(' ' + keyNames[num%keyNames.length]);
+    if (keyOrder.indexOf(key.key) > -1){
+        num = keyOrder.indexOf(key.key);
+        frequency = rootFreq * Math.pow(Math.pow(2,1/12),num);
+        
+        playNote(Math.floor(frequency),duration);
+        logKey(' ' + keyNames[num%keyNames.length],  Math.floor(num/keyNames.length));
+    
+    } else if (shiftKeyOrder.indexOf(key.key) > -1){
+        num = shiftKeyOrder.indexOf(key.key);
+        frequency = rootFreq * Math.pow(Math.pow(2,1/12),num+shiftOffset);
+        
+        playNote(Math.floor(frequency),duration);
+        logKey(' ' + keyNames[num%keyNames.length],  Math.floor(num/keyNames.length));
     } else {
-        if (key.key == 'Backspace'){ document.getElementById('log').innerHTML = '';}
-        else if('1234'.indexOf(key.key) > -1){waveSelected = parseInt(key.key)-1;}
+        if (key.key == 'Backspace'){ 
+            document.getElementById('log').innerHTML = '';
+        }
+        else if('1234'.indexOf(key.key) > -1){
+            waveSelected = parseInt(key.key)-1;
+        }
         else if(key.key == '\\'){
             controlsHidden = document.getElementById('controls').hidden;
             document.getElementById('controls').hidden = !controlsHidden;
@@ -56,19 +82,19 @@ function runKey(key){
             }
         }
 
-        else if (key.key == 'ArrowLeft'){ offsetOctave -= 1;}
-        else if (key.key == 'ArrowRight'){ offsetOctave += 1;}
-
         else if (key.key == 'ArrowUp'){ duration += 10; duration = Math.abs(duration);}
         else if (key.key == 'ArrowDown'){ duration -= 10;  duration = Math.abs(duration);}
         updateInfo();
     }
 }
 
-function toggleTextbox(){
-    textboxHidden = document.getElementById('mobileIn').hidden;
-    document.getElementById('mobileIn').hidden = !textboxHidden;
-}
-
 document.onkeyup = runKey;
-document.onload = updateInfo;
+
+addEventListener("load", function(event) {
+    updateInfo();
+
+    mobileIn = document.getElementById('mobileIn');
+    mobileIn.hidden = !detectMobile();
+    
+});
+
